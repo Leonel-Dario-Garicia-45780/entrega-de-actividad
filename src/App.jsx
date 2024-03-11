15ec368ed1f9891005905ef5b91b040874dd8a65
Leonel Garcia <gun45780@gmail.com>
2024-03-11 12:30:18 -0500

    un comit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import FORmularioclase from "./en clase/formulario"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
